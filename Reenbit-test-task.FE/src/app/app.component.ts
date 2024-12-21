5316e01e8571f56ac58450dd0fb3b81a0736bba9
IvanKarpov-1 <kivan8776w@gmail.com>
2024-12-21 20:09:00 +0200

    feat(FE): define app layout

--- Reenbit-test-task.FE/src/app/app.component.ts
+++ Reenbit-test-task.FE/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
+import { DividerComponent } from './core/layout/divider/divider.component';
+import { MainContentComponent } from './core/layout/main-content/main-content.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SidebarComponent, DividerComponent, MainContentComponent, NgStyle],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Reenbit-test-task.FE';
+  sidebarWidth = signal<number>(400);
+
+  onResizeSidebar(width: number) {
+    this.sidebarWidth.set(width);
+  }
 }
---
+++ Reenbit-test-task.FE/src/app/core/layout/divider/divider.component.ts
@@ -0,0 +1,35 @@
+import { Component, HostListener, output } from '@angular/core';
+
+const SIDEBAR_MIN_WIDTH = 300;
+const SIDEBAR_MAX_WIDTH = 600;
+
+@Component({
+  selector: 'app-divider',
+  standalone: true,
+  imports: [],
+  templateUrl: './divider.component.html',
+  styleUrl: './divider.component.css'
+})
+export class DividerComponent {
+  resizeSidebar = output<number>();
+  private isResizing = false;
+  private sidebarWidth = 400;
+
+  onMouseDown(event: MouseEvent) {
+    this.isResizing = true;
+    event.preventDefault();
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.isResizing) {
+      this.sidebarWidth = Math.max(SIDEBAR_MIN_WIDTH, Math.min(event.clientX, SIDEBAR_MAX_WIDTH));
+      this.resizeSidebar.emit(this.sidebarWidth);
+    }
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    this.isResizing = false;
+  }
+}
